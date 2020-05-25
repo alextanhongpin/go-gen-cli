95a45e77a4fe2f21dc9cebb7ee69abdf31e9ff09
alextanhongpin <alextan220990@gmail.com>
2020-05-25 10:12:19 +0800

    chore: add logic to Config

--- pkg/gen/config.go
+++ pkg/gen/config.go
@@ -1,5 +1,44 @@
 package gen
 
+import "sort"
+
 type Config struct {
 	Templates []*Template `yaml:"templates"`
+}
+
+func (c *Config) Find(name string) *Template {
+	for _, tpl := range c.Templates {
+		if tpl.Name == name {
+			return tpl
+		}
+	}
+	return nil
+}
+
+func (c *Config) Remove(name string) {
+	idx := c.Index(name)
+	if idx == -1 {
+		return
+	}
+	c.Templates = append(c.Templates[:idx], c.Templates[idx+1:]...)
+}
+
+func (c *Config) Index(name string) int {
+	for i, tpl := range c.Templates {
+		if tpl.Name == name {
+			return i
+		}
+	}
+	return -1
+}
+
+func (c *Config) Add(tpl *Template) {
+	c.Templates = append(c.Templates, tpl)
+	c.Sort()
+}
+
+func (c *Config) Sort() {
+	sort.SliceStable(c.Templates, func(i, j int) bool {
+		return c.Templates[i].Name < c.Templates[j].Name
+	})
 }
