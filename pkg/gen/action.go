8f9a6020be36b29fc3e8ede3652d8eec3dde386d
alextanhongpin <alextan220990@gmail.com>
2020-05-25 10:11:55 +0800

    chore: add factory for Action

--- pkg/gen/action.go
+++ pkg/gen/action.go
@@ -1,7 +1,17 @@
 package gen
 
+import "fmt"
+
 type Action struct {
 	Description string `yaml:"description"`
 	Template    string `yaml:"template"`
 	Path        string `yaml:"path"`
+}
+
+func NewAction(name string) *Action {
+	return &Action{
+		Description: fmt.Sprintf("creates a %s", name),
+		Template:    fmt.Sprintf("templates/%s.go"),
+		Path:        fmt.Sprintf("pkg/%s.go"),
+	}
 }
