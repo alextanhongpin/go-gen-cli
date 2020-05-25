a0d7e4eaab1dd6533dc1585ac26b7c7b52a874e8
alextanhongpin <alextan220990@gmail.com>
2020-05-25 11:26:42 +0800

    chore: add chalk to color message

---
+++ pkg/gen/chalk.go
@@ -0,0 +1,19 @@
+package gen
+
+import (
+	"fmt"
+
+	"github.com/ttacon/chalk"
+)
+
+func Warning(msg string) string {
+	return fmt.Sprint(chalk.Red, msg, chalk.Reset)
+}
+
+func Success(msg string) string {
+	return fmt.Sprint(chalk.Green, msg, chalk.Reset)
+}
+
+func Help(msg string) string {
+	return fmt.Sprint(chalk.Blue, msg, chalk.Reset)
+}
